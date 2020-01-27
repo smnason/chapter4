e662850db183d5422005f3b0549e33d6d7116ee7
Shawn Nason <48915912+smnason@users.noreply.github.com>
2020-01-27 12:37:35 -0500

    First comit

---
+++ week4/week4.R
@@ -0,0 +1,54 @@
+best <- function(state,outcome) {
+    
+    setwd("C:\\Users\\Gemma\\Documents\\R\\Projects\\chapter4\\rprog_data_ProgAssignment3-data")
+    
+    #if (is.null(hospitalData)) {
+        
+        hospitalData <<- read.csv("outcome-of-care-measures.csv", stringsAsFactors = FALSE)
+    #}
+
+    stateRows <<- subset(hospitalData, State == state)
+    
+    # testOutcome = "No such results"
+    
+    if (length(stateRows) == 0) {
+        message("There is no such state")
+        #return()
+    }
+    
+    if (outcome == "heart attack") {
+        testOutcome <<- with (stateRows, stateRows$Hospital.Name[ stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == as.character( min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack )), na.rm=TRUE))])
+        #testOutcome <<- with (stateRows, stateRows$Hospital.Name[ stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == as.character( min(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, na.rm=TRUE))])
+        
+        message("heart attack data")
+    }
+    
+    else if (outcome == "heart failure") {
+        testOutcome <<- with (stateRows, stateRows$Hospital.Name[as.numeric( stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure) ==                min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure )), na.rm=TRUE  )])        
+        #testOutcome <<- with (stateRows, stateRows$Hospital.Name[             stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack  == as.character( min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack  )), na.rm=TRUE ))])
+        #testOutcome <<- with (stateRows, stateRows$Hospital.Name[             stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == as.character( min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure )), na.rm=TRUE ))])        
+        #testOutcome <<- with (stateRows, stateRows$Hospital.Name[             stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack  == as.character( min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack  )), na.rm=TRUE ))])
+         
+         
+                
+        #message ( as.character( min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure )), na.rm=TRUE)))
+        message("heart failure data")
+    }
+    
+    else if (outcome == "pneumonia") {
+        #testOutcome <<- with (stateRows, stateRows$Hospital.Name[as.numeric( stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia) == min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia )), na.rm=TRUE)])
+         testOutcome <<- with (stateRows, stateRows$Hospital.Name[            stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == as.character( min(as.numeric(as.character(stateRows$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia )), na.rm=TRUE))])
+        
+        message("pneumonia data")
+    }
+    
+    else {
+        testOutcome <<- NULL
+        message("no such outcomes")
+    }
+    
+    testOutcome
+    
+    return(testOutcome)
+
+}
